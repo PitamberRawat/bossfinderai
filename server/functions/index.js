9783ce8941fa19c4a7237a1b2eb88482f8933a05
PitamberRawat <Pitamberrawat456@gmail.com>
2024-10-18 11:16:00 +0530

    changess

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,3 +1,4 @@
+const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -5,7 +6,7 @@ const Stripe = require("stripe");
 require("dotenv").config();
 
 const app = express();
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY); // Ensure this environment variable is set
+const stripe = Stripe(functions.config().stripe.secret); // Get the Stripe secret from Firebase environment config
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -36,12 +37,9 @@ app.post("/create-checkout-session", async (req, res) => {
         },
       ],
       mode: billingType === "subscription" ? "subscription" : "payment",
-      // success_url:
-      //   "https://bossfinderai.netlify.app/success?session_id={CHECKOUT_SESSION_ID}",
-      // cancel_url: "https://bossfinderai.netlify.app/cancel",
       success_url:
-        "https://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}", // Redirect URL on success
-      cancel_url: "https://localhost:5173/cancel", // Redirect URL on cancel
+        "https://bossfinderai.web.app/success?session_id={CHECKOUT_SESSION_ID}", // Redirect URL on success
+      cancel_url: "https://bossfinderai.web.app/cancel", // Redirect URL on cancel
     });
 
     res.json({ id: session.id }); // Send back the session ID
@@ -50,9 +48,5 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-//bossfinderai.netlify.app
+// Export the Express app as a Firebase function
+exports.api = functions.https.onRequest(app);
